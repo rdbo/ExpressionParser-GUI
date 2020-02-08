ebb551bf4e9a6a90be568913af752f4753a06df0
Rdbo <57117082+rdbo@users.noreply.github.com>
2020-02-08 11:32:09 -0300

    Add files via upload

---
+++ ExpressionParser-GUI/colors.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "pch.h"
+#define BG_R 25
+#define BG_G 25
+#define BG_B 25
+#define BG_BORDER_COLOR_R 25
+#define BG_BORDER_COLOR_G 65
+#define BG_BORDER_COLOR_B 225
+#define BG_BORDER_SIZE 2
+#define COLOR_WHITE ImVec4(1, 1, 1, 1);
+#define COLOR_GRAY ImVec4(0.25, 0.25, 0.25, 1);
+#define COLOR_LIGHT_GRAY ImVec4(0.4, 0.4, 0.4, 1);
+#define COLOR_DARK_BLUE ImVec4(0.1, 0.25, 0.9, 1);
+#define COLOR_BLUE ImVec4(0.2, 0.35, 1, 1);
+#define COLOR_LIGHT_BLUE ImVec4(0.3, 0.4, 1, 1);
---
+++ ExpressionParser-GUI/main.cpp
@@ -0,0 +1,103 @@
+#include "pch.h"
+#include "pdirectx.h"
+#include "colors.h"
+
+WNDCLASSEX wc;
+HWND hwnd;
+RECT wCRect;
+IDirect3D9* pD3D;
+D3DPRESENT_PARAMETERS d3dpp;
+IDirect3DDevice9* pDevice;
+ImGuiWindowFlags wf = 0;
+bool resized;
+
+char expr[1024];
+double output = 0;
+std::stringstream outputstr;
+
+void ImGuiSetupStyle();
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
+{
+	D3D::Init(hInstance, L"ExpressionParser by rdbo");
+	return 0;
+}
+
+bool initstyle = false;
+void D3D::DrawImGui()
+{
+	if (!initstyle)
+	{
+		ImGuiSetupStyle();
+		initstyle = true;
+	}
+	ImGui::Begin("ExpressionParser", 0, wf);
+	ImGui::Text("Enter your expression:");
+	ImGui::InputText("", expr, sizeof(expr));
+	if (ImGui::Button("Parse"))
+	{
+		output = te_interp(expr, 0);
+		outputstr.str("");
+		outputstr.clear();
+		outputstr << "Output: ";
+		outputstr << output;
+	}
+	ImGui::Text(outputstr.str().c_str());
+	ImGui::End();
+}
+
+void ImGuiSetupStyle()
+{
+	wf |= ImGuiWindowFlags_NoTitleBar;
+	wf |= ImGuiWindowFlags_NoMove;
+	wf |= ImGuiWindowFlags_NoResize;
+	wf |= ImGuiWindowFlags_NoCollapse;
+	wf |= ImGuiWindowFlags_NoSavedSettings;
+	wf |= ImGuiWindowFlags_NoBackground;
+	ImGuiStyle& style = ImGui::GetStyle();
+	ImVec4* colors = style.Colors;
+	colors[ImGuiCol_Text] = COLOR_WHITE;
+	colors[ImGuiCol_FrameBg] =
+		colors[ImGuiCol_FrameBgHovered] =
+		colors[ImGuiCol_FrameBgActive] = COLOR_GRAY;
+	colors[ImGuiCol_Button] = COLOR_DARK_BLUE;
+	colors[ImGuiCol_ButtonHovered] = COLOR_BLUE;
+	colors[ImGuiCol_ButtonActive] = COLOR_LIGHT_BLUE;
+}
+
+extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK D3D::WindowProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
+		return true;
+
+	switch (msg)
+	{
+	case WM_SIZE:
+		if (pDevice != NULL && wParam != SIZE_MINIMIZED)
+		{
+			d3dpp.BackBufferWidth = LOWORD(lParam);
+			d3dpp.BackBufferHeight = HIWORD(lParam);
+			ResetDevice();
+			GetClientRect(hwnd, &wCRect);
+			resized = true;
+		}
+		return 0;
+
+	case WM_GETMINMAXINFO:
+	{
+		LPMINMAXINFO lpMMI = (LPMINMAXINFO)lParam;
+		lpMMI->ptMinTrackSize.x = MIN_WIDTH;
+		lpMMI->ptMinTrackSize.y = MIN_HEIGHT;
+		break;
+	}
+	case WM_SYSCOMMAND:
+		if ((wParam & 0xfff0) == SC_KEYMENU)
+			return 0;
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return ::DefWindowProc(hWnd, msg, wParam, lParam);
+}
